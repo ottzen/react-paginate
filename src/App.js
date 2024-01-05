66d03d81288f5e031adc9de08c5f108e190952d6
Kasper Ottzen <kasper@ottzen.com>
2024-01-05 10:42:04 +0100

    Page showing pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const itemsPerPage = 10;
+
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const subset = data.slice(startIndex, endIndex);
+
+  const handlePageChange = (selectedPage) => {
+    setCurrentPage(selectedPage.selected);
+};
+     
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
+      setData(response.data);
+      setTotalPages(Math.ceil(response.data.length / itemsPerPage))
+    });
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {subset.map((item) => (
+            <div key={item.id}>{item.title}</div>
+        ))}
+        <ReactPaginate
+            pageCount={totalPages}
+            onPageChange={handlePageChange}
+            forcePage={currentPage}
+            previousLabel={"<<"}
+            containerClassName={"pagination"}          
+            nextLabel={">>"}
+            previousLinkClassName={"pagination__link"}
+            nextLinkClassName={"pagination__link"}
+            disabledClassName={"pagination__link--disabled"}
+            activeClassName={"pagination__link--active"}
+        />
+      </div>
     </div>
   );
 }
